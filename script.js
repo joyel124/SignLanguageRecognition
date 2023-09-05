0827a2d4255ebe449fb1c708995947fe631443b1
joyel124 <geolrco124@hotmail.com>
2023-09-05 15:10:07 -0500

    feat: Updated logical.

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-// Copyright 2023 The MediaPipe Authors.
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//      http://www.apache.org/licenses/LICENSE-2.0
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 import { GestureRecognizer, FilesetResolver } from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0";
 const demosSection = document.getElementById("demos");
 let gestureRecognizer;
@@ -121,7 +111,10 @@ function enableCam(event) {
     }
     // getUsermedia parameters.
     const constraints = {
-        video: true
+        audio: false,
+        video: {
+            facingMode: "environment",
+        }
     };
     // Activate the webcam stream.
     navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
